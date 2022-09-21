7719ce3e253725c5a51fe0435cfbc1219de228ff
Gabriel Paiva Rio <gabrielpaivario@gmail.com>
2022-09-20 22:16:06 -0300

    adicionando estrutura inicial do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import LandingPage from "./pages/Landingpage"
+
+function App() {
+  return (
+    <LandingPage></LandingPage>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+    return(
+        <>
+        Teste
+        </>
+    )
+}
+
+export default LandingPage;
